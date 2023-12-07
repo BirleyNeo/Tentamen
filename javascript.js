0e53694f3836285dcff7085a174dc600b3a84526
BirleyNeo <142905797+BirleyNeo@users.noreply.github.com>
2023-12-07 14:35:41 +0100

    AHHHHH JAVA

--- javascript.js
+++ javascript.js
@@ -1,65 +1,41 @@
-let article1 = document.querySelector("#article1")
-    article1.style.backgroundColor = "lightblue"
-
-function setBackground (answ) {
-
+function setBackground(answ) {
+    let sb = document.getElementById("idsecondbox");
     if (answ == "Red") {
 
-        article1.style.backgroundColor = "darkred"
+        sb.style.backgroundColor = "darkred"
 
     }
 
     else if (answ == "Green") {
 
-        article1.style.backgroundColor = "darkgreen"
-
-    }  
-
-    else if (answ == "Blue") {
-
-        article1.style.backgroundColor = "darkblue"
+        sb.style.backgroundColor = "darkgreen"
 
     }
 
-}
+    else if (answ == "Blue") {
 
-let bkChoice = document.querySelectorAll(".bgcolor")
+        sb.style.backgroundColor = "darkblue"
 
-console.log(bkChoice)
+    }
 
-bkChoice.forEach(element => {
+}; 
 
-    element.addEventListener("click", () => {
-
-        let answ = element.innerHTML
-
-        console.log(answ)
-
-        setBackground(answ)
-
-    })
-
-});
-
-document.getElementById('copy-button').addEventListener('click', function() { 
-    var text = document.getElementById('text-box').value; 
-    document.getElementById('text-output').textContent = text; 
-}); 
-
-function getRandomOutputElement() { 
-    var outputs = document.getElementsByClassName('text-output'); 
-    var randomIndex = Math.floor(Math.random() * outputs.length); 
-    return outputs[randomIndex]; 
-} 
-document.getElementById('copy-button').addEventListener('click', function() { 
-    var text = document.getElementById('text-box').value; 
-    var randomOutputElement = getRandomOutputElement(); 
-    randomOutputElement.textContent = text; 
-});
-
-document.getElementById('copy-button').addEventListener('click', function() { 
-    var targets = document.querySelectorAll('.text-output');
-    var randomTarget = targets[Math.floor(Math.random() * targets.length)]; 
-    var text = document.getElementById('text-box').value; 
-    randomTarget.textContent = text; 
+function copyText() {
+    document.getElementById("idsecondbox").textContent= document.getElementById("idfirstbox").value;
+};
+  
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("mybutton").addEventListener("click", copyText);
+    document.getElementById("red").addEventListener("click", () => {
+        setBackground("Red");
+    });
+    document.getElementById("green").addEventListener("click", () => {
+        setBackground("Green");
+    });
+    document.getElementById("blue").addEventListener("click", () => {
+        setBackground("Blue");
+    });
+    document.getElementById("font").addEventListener ("click", () => {
+        document.getElementById ("idsecondbox").style.fontFamily = "monospace";
+    });
 });
